f892aa23ebe4d5b4193934aa96c3f75d85760595
Blake <81705490+blakebdavies@users.noreply.github.com>
2022-10-31 21:12:36 +0000

    added buy now functionality

--- pages/success.js
+++ pages/success.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useEffect} from 'react';
 import Link from 'next/link';
 import {BsBagCheckFill} from "react-icons/bs";
 
@@ -9,12 +9,13 @@ const Success = () => {
     const {setCartItems, setTotalPrice, setTotalQuantities} = useStateContext();
 
     useEffect(() => {
-        localStorage.clear();
-        setCartItems([]);
-        setTotalPrice(0);
-        setTotalQuantities(0);
-        runFireworks();
-    }, []);
+            localStorage.clear();
+            setCartItems([]);
+            setTotalPrice(0);
+            setTotalQuantities(0);
+            runFireworks();
+        },
+        []);
     return (
         <div className='success-wrapper'>
             <div className="success">
@@ -28,7 +29,7 @@ const Success = () => {
                     <a className="email" href="mailto:order@example.com">order@example.com</a>
                 </p>
                 <Link href="/">
-                    <button type="button" width="300px" className="btn">Continue Shopping</button>
+                    <button type="button" className="btn">Continue Shopping</button>
                 </Link>
             </div>
         </div>
