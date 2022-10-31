c8cb07f0558ca9ff10a89eee8edb19b40f401458
Blake <81705490+blakebdavies@users.noreply.github.com>
2022-10-31 15:06:57 +0000

    Added quantity functionality to cart component

--- context/StateContext.js
+++ context/StateContext.js
@@ -10,15 +10,18 @@ export const StateContext = ({children}) => {
     const [totalQuantities, setTotalQuantities] = useState(0);
     const [qty, setQty] = useState(1);
 
+    let foundProduct;
+    let index;
+
     const onAdd = (product, quantity) => {
         const checkProductInCart = cartItems.find((item) => item._id === product._id);
 
         setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity);
         setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity);
 
-        if(checkProductInCart) {
+        if (checkProductInCart) {
             const updatedCartItems = cartItems.map((cartProduct) => {
-                if(cartProduct._id === product._id) return {
+                if (cartProduct._id === product._id) return {
                     ...cartProduct,
                     quantity: cartProduct.quantity + quantity
                 }
@@ -28,10 +31,26 @@ export const StateContext = ({children}) => {
         } else {
             product.quantity = quantity;
 
-            setCartItems([...cartItems, { ...product }]);
+            setCartItems([...cartItems, {...product}]);
         }
 
         toast.success(`${qty} ${product.name} added to the cart.`);
+    }
+
+    const toggleCartItemQuantity = (id, value) => {
+        foundProduct = cartItems.find((item) => item._id === id)
+        index = cartItems.findIndex((product) => product._id === id);
+        const newCartItems = cartItems.filter((item) => item._id !== id)
+
+        if(value === 'inc') {
+            setCartItems(cartItems.map((item) => item._id === id ? { ...foundProduct, quantity: foundProduct.quantity + 1 } : item));            setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price)
+            setTotalQuantities(prevTotalQuantities => prevTotalQuantities + 1)
+        } else if(value === 'dec') {
+            if (foundProduct.quantity > 1) {
+                setCartItems(cartItems.map((item) => item._id === id ? { ...foundProduct, quantity: foundProduct.quantity - 1 } : item));                setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price)
+                setTotalQuantities(prevTotalQuantities => prevTotalQuantities - 1)
+            }
+        }
     }
 
     const incQty = () => {
@@ -58,6 +77,7 @@ export const StateContext = ({children}) => {
                 incQty,
                 decQty,
                 onAdd,
+                toggleCartItemQuantity,
             }}
         >
             {children}
